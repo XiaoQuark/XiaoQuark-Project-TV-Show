ff7c36a65a3c386d22eff9dfc8f41204334324a1
XiaoQuark <kris.oldrini@gmail.com>
2026-04-11 08:31:52 +0100

    coursework: create episode card template and start injecting name, series, episode number and summary

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 //You can edit ALL of the code here
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+	const allEpisodes = getAllEpisodes();
+	makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+	const rootElem = document.getElementById("root");
+	rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+	for (const episode of episodeList) {
+		const card = createEpisodeCard(episode);
+		console.log(card);
+		document.body.appendChild(card);
+	}
+}
+
+function createEpisodeCard(episode) {
+	const card = document
+		.getElementById("episode-card")
+		.content.cloneNode(true);
+
+	card.querySelector("h2").textContent = episode.name;
+	card.querySelector("[season-number]").textContent = episode.season;
+	card.querySelector("[episode-number]").textContent = episode.number;
+	card.querySelector("[summary]").innerHTML = episode.summary;
+
+	return card;
 }
 
 window.onload = setup;
