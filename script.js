4b380187ba77977b6f898beb680daad62490673a
Kris Oldrini <kris.oldrini@gmail.com>
2026-04-11 14:18:11 +0100

    refactor: move section and template inside root and refactor makePageForEpisodes()

--- script.js
+++ script.js
@@ -1,44 +1,40 @@
 //You can edit ALL of the code here
 function setup() {
-	const allEpisodes = getAllEpisodes();
-	makePageForEpisodes(allEpisodes);
+  const allEpisodes = getAllEpisodes();
+  makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
-	const rootElem = document.getElementById("root");
-	rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-	for (const episode of episodeList) {
-		const card = createEpisodeCard(episode);
-		console.log(card);
-		document.body.appendChild(card);
-	}
+  for (const episode of episodeList) {
+    const card = createEpisodeCard(episode);
+    console.log(card);
+    document.body.appendChild(card);
+  }
 }
 
 function createEpisodeCard(episode) {
-	const card = document
-		.getElementById("episode-card")
-		.content.cloneNode(true);
+  const card = document.getElementById("episode-card").content.cloneNode(true);
 
-	const image = card.querySelector("img");
-	image.src = episode.image.medium;
-	image.alt = `Screenshot from episode ${episode.name}`;
-	card.querySelector("h2").textContent = episode.name;
-	card.querySelector("[episode-code]").textContent = createEpisodeCode(
-		episode.season,
-		episode.number,
-	);
-	card.querySelector("[season-number]").textContent =
-		`Season number: ${episode.season}`;
-	card.querySelector("[episode-number]").textContent =
-		`Episode number: ${episode.number}`;
-	card.querySelector("[summary]").innerHTML = episode.summary;
+  const image = card.querySelector("img");
+  image.src = episode.image.medium;
+  image.alt = `Screenshot from episode ${episode.name}`;
+  card.querySelector("h2").textContent = episode.name;
+  card.querySelector("[episode-code]").textContent = createEpisodeCode(
+    episode.season,
+    episode.number,
+  );
+  card.querySelector("[season-number]").textContent =
+    `Season number: ${episode.season}`;
+  card.querySelector("[episode-number]").textContent =
+    `Episode number: ${episode.number}`;
+  card.querySelector("[summary]").innerHTML = episode.summary;
 
-	return card;
+  return card;
 }
 
 function createEpisodeCode(season, number) {
-	console.log(season, number);
-	return `S${season.toString().padStart(2, "0")}E${number.toString().padStart(2, "0")}`;
+  console.log(season, number);
+  return `S${season.toString().padStart(2, "0")}E${number.toString().padStart(2, "0")}`;
 }
 
 window.onload = setup;
