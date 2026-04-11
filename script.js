62015e4127e781a40c2291b23835be53aadac1af
XiaoQuark <kris.oldrini@gmail.com>
2026-04-11 08:38:41 +0100

    coursework: finish inject episode info, image and episode code

--- script.js
+++ script.js
@@ -19,12 +19,26 @@ function createEpisodeCard(episode) {
 		.getElementById("episode-card")
 		.content.cloneNode(true);
 
+	const image = card.querySelector("img");
+	image.src = episode.image.medium;
+	image.alt = `Screenshot from episode ${episode.name}`;
 	card.querySelector("h2").textContent = episode.name;
-	card.querySelector("[season-number]").textContent = episode.season;
-	card.querySelector("[episode-number]").textContent = episode.number;
+	card.querySelector("[episode-code]").textContent = createEpisodeCode(
+		episode.season,
+		episode.number,
+	);
+	card.querySelector("[season-number]").textContent =
+		`Season number: ${episode.season}`;
+	card.querySelector("[episode-number]").textContent =
+		`Episode number: ${episode.number}`;
 	card.querySelector("[summary]").innerHTML = episode.summary;
 
 	return card;
+}
+
+function createEpisodeCode(season, number) {
+	console.log(season, number);
+	return `S${season.toString().padStart(2, "0")}E${number.toString().padStart(2, "0")}`;
 }
 
 window.onload = setup;
